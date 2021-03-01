acd4cb6551184c456ad3870535bfc1fccf833f93
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-01 07:11:15 +0000

    [1. rails sができない]が解決

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :users, only: [:show, :index, :edit, :update]
   resources :books
+
   devise_for :users
+
   root 'homes#top'
+
   get 'home/about' => 'homes#about'
+
+end
