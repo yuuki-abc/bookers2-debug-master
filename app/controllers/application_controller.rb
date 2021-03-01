5e937862b8fb9e589c2689a9e41143bd90b79622
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-01 08:01:56 +0000

    [6. ヘッダーのhome,aboutアイコンを押してもLoginの画面のまま]を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:home, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def after_sign_in_path_for(resource)
     root_path
   end
